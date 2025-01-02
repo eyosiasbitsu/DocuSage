1b7ec0c525a5159dd8e742a665793e87013398cc
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2025-01-03 00:45:45 +0300

    Update main.py

--- docusage-backend/main.py
+++ docusage-backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.routers.Auth.router import auth_router
-
+import uvicorn
 import os
+
 
 app = FastAPI()
 
@@ -9,10 +10,9 @@ app.include_router(auth_router, prefix="/api/v1/auth")
 
 
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Use PORT from environment or default to 8000
 
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 #Hostname = dpg-ctrfm2lds78s739l6lv0-a
